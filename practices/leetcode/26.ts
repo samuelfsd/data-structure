b0eed31de994dfba571aec538588d3ba38a3b689
samuelfsd <samuel.estudo778@gmail.com>
2024-12-22 17:33:08 -0300

    chore: adiciona nova configuração do repo

---
+++ practices/leetcode/26.ts
@@ -0,0 +1,12 @@
+// 26. Remove Duplicates from Sorted Array
+
+function removeDuplicates(nums: number[]): number {
+    let count = 0
+    for(let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i+1]) {
+            nums[count] = nums[i]
+            count++
+        }
+    }
+    return count
+};
